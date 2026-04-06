a2350f8294b2cfd9cf69498d0f66a936ba87537a
karl robeck alferez <karlalferezfx@gmail.com>
2026-04-06 20:52:38 +0800

    feat: update RusqliteRow get method to support Null type and extend return type to Either5

--- src/row.rs
+++ src/row.rs
@@ -1,6 +1,6 @@
 use napi::{
-  bindgen_prelude::{Buffer, Either3, Either4, ToNapiValue},
-  Either, Env, JsValue, Unknown,
+  bindgen_prelude::{Buffer, Either5, Null},
+  Either,
 };
 use napi_derive::napi;
 
@@ -14,7 +14,10 @@ pub struct RusqliteRow<'a> {
 #[napi]
 impl<'a> RusqliteRow<'a> {
   #[napi]
-  pub fn get(&self, index: Either<String, i64>) -> napi::Result<Either4<String, i64, f64, Buffer>> {
+  pub fn get(
+    &self,
+    index: Either<String, i64>,
+  ) -> napi::Result<Either5<String, i64, f64, Buffer, Null>> {
     let result = match index {
       Either::A(string) => self.row.get_ref(&*string),
       Either::B(number) => self.row.get_ref(number as usize),
@@ -24,11 +27,11 @@ impl<'a> RusqliteRow<'a> {
     let r#type = result.data_type();
 
     let value = match r#type {
-      rusqlite::types::Type::Text => Either4::A(result.as_str().unwrap().to_string()),
-      rusqlite::types::Type::Integer => Either4::B(result.as_i64().unwrap()),
-      rusqlite::types::Type::Real => Either4::C(result.as_f64().unwrap()),
-      rusqlite::types::Type::Blob => Either4::D(Buffer::from(result.as_blob().unwrap())),
-      _ => panic!("null and blob not implemented yet"),
+      rusqlite::types::Type::Text => Either5::A(result.as_str().unwrap().to_string()),
+      rusqlite::types::Type::Integer => Either5::B(result.as_i64().unwrap()),
+      rusqlite::types::Type::Real => Either5::C(result.as_f64().unwrap()),
+      rusqlite::types::Type::Blob => Either5::D(Buffer::from(result.as_blob().unwrap())),
+      rusqlite::types::Type::Null => Either5::E(Null),
     };
 
     Ok(value)
